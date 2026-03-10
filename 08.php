93f200d5134066d0dfeafbe53e1e360444945c1e
Paulo1073 <paulo.silva1073@aluno.ce.gov.br>
2026-03-10 16:02:44 -0300

    Alterações

--- 08.php
+++ 08.php
@@ -1,30 +1,35 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Conversão Dólar para Real</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 <body>
 
-<h2>Conversão de Dólar para Real</h2>
+<form method="POST">
+        <label>Digite o valor da cotação do dólar:</label>
+        <input type="number" name="cotd" step="0.1">
+        <br>
+        <label>Digite quantos dólares possui:</label>
+        <input type="number" name="dola" step="0.1">
+        <br>
+        <button type="submit">Enviar valores</button>
+    </form>
+    <br>
 
-<form method="post">
-    Cotação do dólar:
-    <input type="text" name="cotacao"><br><br>
-    Quantidade de dólares: <input type="text" name="dolares"><br><br>
-    <input type="submit" value="Converter">
-</form>
+    <?php
 
-<?php
-if(isset($_POST['cotacao']) && isset($_POST['dolares'])){
-
-    $cotacao = $_POST['cotacao'];
-    $dolares = $_POST['dolares'];
-
-    $reais = $cotacao * $dolares;
-
-    echo "<h3>Valor em Reais: R$ $reais</h3>";
-}
-?>
-
+    if ($_POST){
+        $cotacao = $_POST['cotd'];
+        $dollar = $_POST['dola'];
+        $reais = (float)$dollar * (float)$cotacao;
+        echo "O valor da cotação é: " . $cotacao . "<br>";
+        echo "O valor em dólares é: " . $dollar . "<br>";
+        echo "O valor em reais é: " . $reais;
+    }
+    
+    ?>
+    
 </body>
 </html>
